a806df6e32500949cf294b035c19815e6046316d
alalulu8668 <wllei@kth.se>
2023-10-11 17:19:27 +0200

    use simpleaichat for bot, add vectordb for bmz.

--- bot/create_vectordb.py
+++ bot/create_vectordb.py
@@ -10,14 +10,14 @@ from langchain.agents.agent_toolkits import (
     VectorStoreToolkit,
     VectorStoreInfo,
 )
-
+from simpleaichat import AIChat
 # llm = OpenAI(temperature=0)
 # Load the text file
 # loader = TextLoader("/home/alalulu/workspace/chatbot_bmz/chatbot/text_files/CODE_OF_CONDUCT.txt")
 
 
 # Read text_files folder to get all txt files including the ones in subfolders
-def text_to_vectorstore(root_folder):
+def md_to_vectorstore(root_folder):
     txt_list = []
     for foldername, subfolders, filenames in os.walk(root_folder):
             for filename in filenames:
@@ -27,35 +27,37 @@ def text_to_vectorstore(root_folder):
                     loader = TextLoader(file_path)
                     documents = loader.load()
 
-                    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+                    text_splitter = CharacterTextSplitter(separator="\n## ", chunk_size=1000, chunk_overlap=50)
                     texts=text_splitter.split_documents(documents)
                     txt_list.append(texts)
     return txt_list
 root_folder = "/home/alalulu/workspace/chatbot_bmz/bioimage.io/docs"# "/home/alalulu/workspace/chatbot_bmz/chatbot/text_files"
-txt_list = text_to_vectorstore(root_folder)
+txt_list = md_to_vectorstore(root_folder)
 texts = [item for sublist in txt_list for item in sublist]
 embeddings = OpenAIEmbeddings()
-docs_store = Chroma.from_documents(
-    texts, embeddings, collection_name="bioimage.io-docs"
-)
+output_dir="docs/vectordb"
+# docs_store = Chroma.from_documents(
+#     texts, embeddings, persist_directory=output_dir, collection_name="bioimage.io-docs"
+# )
 
-vectorstore_info = VectorStoreInfo(
-    name="BioImage Model Zoo",
-    description="documentation from bioimage.io.",
-    vectorstore=docs_store,
-)
-llm = OpenAI(temperature=0)
-toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-agent_executor = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=True)
 
-response = agent_executor.run(
-    "Model contribution guidelines"
-)
+
+PREFIX = """Your name is BMZ. You are an chatbot designed to answer questions about sets of documents. 
+If you are given a set of raw documents retrieved from a search engine in the `Context`, you will answer the question based on the `Context`.
+If the question does not seem to be relavant to the `Context`, just return "I don't know" as the answer.
+"""
+
+ai = AIChat(system=PREFIX)
+
+question = "what are Model contribution guidelines?"
+docs = docs_store.similarity_search(question)
+raw_docs = []
+for doc in docs:
+    # combine all the docs into one string
+    raw_docs.append("```markdown\n" + doc.page_content + "\n```")
+raw_docs = "\n".join(raw_docs)
+
+prompt = f"##Context\n{raw_docs}\n##Question\n{question}\nNow, please anwser the user's question based the context."
+response = ai(prompt)
 print(response)
 
-
-
-
-# query = "can you show me the complete Model contribution guidelines?"
-# docs = state_of_union_store.similarity_search(query)
-# print(docs[0].page_content)
--- bot/start-BMZ-chatbot.py
+++ bot/start-BMZ-chatbot.py
@@ -16,45 +16,65 @@ from langchain.agents.agent_toolkits import (
     VectorStoreToolkit,
     VectorStoreInfo,
 )
-
-llm = OpenAI(temperature=0)
+from simpleaichat import AIChat
 from langchain.document_loaders import TextLoader
 
-# loader = TextLoader("/home/alalulu/workspace/hypha-bot/scripts/imageJ_doc.txt")
-loader = TextLoader("/home/alalulu/workspace/chatbot_bmz/chatbot/text_files/CODE_OF_CONDUCT.txt")  
+# # loader = TextLoader("/home/alalulu/workspace/hypha-bot/scripts/imageJ_doc.txt")
+# loader = TextLoader("/home/alalulu/workspace/hypha-bot/scripts/imageJ_doc.txt")  
 
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+# documents = loader.load()
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# texts = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings()
-state_of_union_store = Chroma.from_documents(
-    texts, embeddings, collection_name="state-of-union"
-)
-
-
-vectorstore_info = VectorStoreInfo(
-    name="imageJ_database",
-    description="documentation of imageJ",
-    vectorstore=state_of_union_store,
-)
-toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-agent_executor = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=True)
-
-# response = agent_executor.run(
-#     "what is imagej"
+# embeddings = OpenAIEmbeddings()
+# docs_store = Chroma.from_documents(
+#     texts, embeddings, collection_name="state-of-union"
 # )
 
-# print(response)
+
+# vectorstore_info = VectorStoreInfo(
+#     name="imageJ_database",
+#     description="documentation of imageJ",
+#     vectorstore=docs_store,
+# )
+# toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+# agent_executor = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=True)
+
+# # response = agent_executor.run(
+# #     "what is imagej"
+# # )
+
+# # print(response)
+embeddings = OpenAIEmbeddings()
+output_dir="docs/vectordb"
+docs_store = Chroma(collection_name="bioimage.io-docs", persist_directory=output_dir, embedding_function=embeddings)
+
+
+PREFIX = """Your name is BMZ. You are an chatbot designed to answer questions about sets of documents. 
+If you are given a set of raw documents retrieved from a search engine in the `Context`, you will answer the question based on the `Context`.
+If the question does not seem to be relavant to the `Context`, just return "I don't know" as the answer.
+"""
+
+
 async def start_server(server_url):
     token = await login({"server_url": server_url})
     server = await connect_to_server({"server_url": server_url, "token": token})
+    ai = AIChat(system=PREFIX)
+
     # llm = OpenAI(temperature=0.9)
     
     async def chat(text, api, context=None):
-        response = agent_executor.run(
-            text
-        )
+        # response = agent_executor.run(
+        #     text
+        # )
+        docs = docs_store.similarity_search(text)
+        raw_docs = []
+        for doc in docs:
+            # combine all the docs into one string
+            raw_docs.append("```markdown\n" + doc.page_content + "\n```")
+        raw_docs = "\n".join(raw_docs)
+        prompt = f"##Context\n{raw_docs}\n##Question\n{text}\nNow, please anwser the user's question based the context. The response will render as markdown."
+        response = ai(prompt)
         print(f"\nUser: {text}\nBot: {response}")
         return response
 
@@ -67,9 +87,6 @@ async def start_server(server_url):
         },
         "chat": chat
     })
-
-    # print(f"hello world service regisered at workspace: {server.config.workspace}")
-    # print(f"Test it with the http proxy: {server_url}/{server.config.workspace}/services/hello-world/hello?name=John")
 
 if __name__ == "__main__":
     server_url = "https://ai.imjoy.io"
