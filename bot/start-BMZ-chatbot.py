61b3a45bd0da2a179985d4439a03a6eade38f48a
alalulu8668 <wllei@kth.se>
2023-10-11 18:00:43 +0200

    modify search to k=2

--- bot/start-BMZ-chatbot.py
+++ bot/start-BMZ-chatbot.py
@@ -19,32 +19,6 @@ from langchain.agents.agent_toolkits import (
 from simpleaichat import AIChat
 from langchain.document_loaders import TextLoader
 
-# # loader = TextLoader("/home/alalulu/workspace/hypha-bot/scripts/imageJ_doc.txt")
-# loader = TextLoader("/home/alalulu/workspace/hypha-bot/scripts/imageJ_doc.txt")  
-
-# documents = loader.load()
-# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# texts = text_splitter.split_documents(documents)
-
-# embeddings = OpenAIEmbeddings()
-# docs_store = Chroma.from_documents(
-#     texts, embeddings, collection_name="state-of-union"
-# )
-
-
-# vectorstore_info = VectorStoreInfo(
-#     name="imageJ_database",
-#     description="documentation of imageJ",
-#     vectorstore=docs_store,
-# )
-# toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-# agent_executor = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=True)
-
-# # response = agent_executor.run(
-# #     "what is imagej"
-# # )
-
-# # print(response)
 embeddings = OpenAIEmbeddings()
 output_dir="docs/vectordb"
 docs_store = Chroma(collection_name="bioimage.io-docs", persist_directory=output_dir, embedding_function=embeddings)
@@ -53,6 +27,7 @@ docs_store = Chroma(collection_name="bioimage.io-docs", persist_directory=output
 PREFIX = """Your name is BMZ. You are an chatbot designed to answer questions about sets of documents. 
 If you are given a set of raw documents retrieved from a search engine in the `Context`, you will answer the question based on the `Context`.
 If the question does not seem to be relavant to the `Context`, just return "I don't know" as the answer.
+For example, if the user asks "who are you?", you should return "I am BMZ, a chatbot designed to answer questions about sets of documents."
 """
 
 
@@ -67,7 +42,7 @@ async def start_server(server_url):
         # response = agent_executor.run(
         #     text
         # )
-        docs = docs_store.similarity_search(text)
+        docs = docs_store.similarity_search(text,k=2)
         raw_docs = []
         for doc in docs:
             # combine all the docs into one string
@@ -87,7 +62,7 @@ async def start_server(server_url):
         },
         "chat": chat
     })
-
+    print("visit this to test the bot: https://jsfiddle.net/gzyradL5/8/")
 if __name__ == "__main__":
     server_url = "https://ai.imjoy.io"
     loop = asyncio.get_event_loop()
