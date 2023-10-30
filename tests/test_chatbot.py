321c232dd0f80204986db8c290f281c1e0c9ee5c
Wei Ouyang <oeway007@gmail.com>
2023-10-30 13:44:49 +0100

    Rename instruct_content to data

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -14,22 +14,22 @@ async def test_chatbot():
     question = "Which tool can I use to analyse western blot image?"
     profile = UserProfile(name="lulu", occupation="data scientist", background="machine learning and AI")
     m = QuestionWithHistory(question=question, chat_history=chat_history, user_profile=UserProfile.parse_obj(profile), channel_id=None)
-    resp = await customer_service.handle(Message(content=m.json(), instruct_content=m , role="User"))
+    resp = await customer_service.handle(Message(content=m.json(), data=m , role="User"))
     assert resp
 
     question = "Which tool can I use to segment an cell image?"
     profile = UserProfile(name="lulu", occupation="data scientist", background="machine learning and AI")
     m = QuestionWithHistory(question=question, chat_history=chat_history, user_profile=UserProfile.parse_obj(profile), channel_id=None)
-    resp = await customer_service.handle(Message(content=m.json(), instruct_content=m , role="User"))
+    resp = await customer_service.handle(Message(content=m.json(), data=m , role="User"))
     assert resp
     
     question = "How can I test the models?"
     profile = UserProfile(name="lulu", occupation="data scientist", background="machine learning and AI")
     m = QuestionWithHistory(question=question, chat_history=chat_history, user_profile=UserProfile.parse_obj(profile), channel_id="bioimage.io")
-    resp = await customer_service.handle(Message(content=m.json(), instruct_content=m , role="User"))
+    resp = await customer_service.handle(Message(content=m.json(), data=m , role="User"))
     assert resp
 
     question = "What are Model Contribution Guidelines?"
     m = QuestionWithHistory(question=question, chat_history=chat_history, user_profile=UserProfile.parse_obj(profile))
-    resp = await customer_service.handle(Message(content=m.json(), instruct_content=m , role="User"))
+    resp = await customer_service.handle(Message(content=m.json(), data=m , role="User"))
     assert resp
