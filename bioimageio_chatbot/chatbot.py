67d744692ddcb994c0805ce93ab6f530c6944f25
alalulu8668 <wllei@kth.se>
2023-11-12 00:03:27 +0100

    updates from comments.

--- bioimageio_chatbot/chatbot.py
+++ bioimageio_chatbot/chatbot.py
@@ -317,7 +317,7 @@ async def register_chat_service(server):
 
 if __name__ == "__main__":
     # asyncio.run(main())
-    server_url = "https://chat.bioimage.io"
+    server_url = "https://ai.imjoy.io"
     loop = asyncio.get_event_loop()
     loop.create_task(connect_server(server_url))
     loop.run_forever()
