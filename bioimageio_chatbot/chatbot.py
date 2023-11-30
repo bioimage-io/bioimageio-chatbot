daa60411b7b81f9890ed8f694b2d1afeef77b417
alalulu8668 <wllei@kth.se>
2023-11-30 07:16:54 +0100

    add file selection for image

--- bioimageio_chatbot/chatbot.py
+++ bioimageio_chatbot/chatbot.py
@@ -16,6 +16,7 @@ import io
 from bioimageio_chatbot.knowledge_base import load_knowledge_base
 from bioimageio_chatbot.utils import get_manifest
 import pkg_resources
+import base64
 
 def load_model_info():
     response = requests.get("https://bioimage-io.github.io/collection-bioimage-io/collection.json")
@@ -309,7 +310,7 @@ async def register_chat_service(server):
         await save_chat_history(chat_log_full_path, chat_his_dict)
         print(f"User report saved to {filename}")
         
-    async def chat(text, chat_history, user_profile=None, channel=None, status_callback=None, session_id=None, context=None):
+    async def chat(text, chat_history, user_profile=None, channel=None, status_callback=None, session_id=None, imageData=None,context=None):
         if login_required and context and context.get("user"):
             assert check_permission(context.get("user")), "You don't have permission to use the chatbot, please sign up and wait for approval"
         session_id = session_id or secrets.token_hex(8)
@@ -320,7 +321,26 @@ async def register_chat_service(server):
                     await status_callback(message.dict())
 
         event_bus.on("stream", stream_callback)
-        
+        # decode image 
+        def decode_base64(encoded_data):
+            try:
+                decoded_data = base64.b64decode(encoded_data)
+                return decoded_data
+            except Exception as e:
+                print(f"Error decoding base64: {e}")
+                return None
+
+        # Example usage:
+        # base64_encoded_image = "your_base64_encoded_image_data_here"
+        decoded_image_data = decode_base64(imageData)
+
+        if decoded_image_data is not None:
+            # Now you can use 'decoded_image_data' as the binary image data
+            # with open("decoded_image.jpg", "wb") as image_file:
+            #     image_file.write(decoded_image_data)
+            print("Image successfully decoded and saved.")
+        else:
+            print("Image decoding failed.")
         # Get the channel id by its name
         if channel == 'auto':
             channel = None
