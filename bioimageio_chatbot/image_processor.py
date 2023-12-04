091b5d12a7e3b95dbfe56da3ef08885169da69f5
Gabriel Reder <gkreder@gabrielrmbp.dyn.scilifelab.se>
2023-12-04 15:58:38 +0100

    tweaking image embedding and test

--- bioimageio_chatbot/image_processor.py
+++ bioimageio_chatbot/image_processor.py
@@ -84,7 +84,11 @@ class ImageProcessor():
         except Exception as e:
             raise Exception(f"Error loading image from {self.image_path}: {str(e)}")
 
-    def resize_image(self, input_image, current_format, output_format = "byxc"):
+
+    def paint_image(input_image, input_format = "bcxy", output_channels = 3):
+        
+
+    def resize_image(self, input_image, current_format, output_format = "byxc", output_dims_xy = (224,224)):
         # input_image = np.load(input_image_path)
         # self.load_image(input_image_path)
         # input_image = self.image
@@ -96,26 +100,32 @@ class ImageProcessor():
             input_image = np.expand_dims(input_image, axis=0)
             current_axes = 'c' + current_axes
         if 'z' in current_axes:
-            input_image = np.mean(input_image, current_format.index('z'))
+            input_image = np.mean(input_image, current_format.index('z'), keepdims='z' in output_format)
             current_axes = current_axes.replace('z', '')
-        assert ''.join(sorted(current_axes)) == 'bcxy'
-        index_tup = tuple(current_axes.index(c) for c in 'bcyx') # reshape to bcyx
+        output_format_sorted = ''.join(sorted(output_format))
+        assert ''.join(sorted(current_axes)) == output_format_sorted
+        index_tup = tuple(current_axes.index(c) for c in output_format_sorted) # reshape to bcyx
         input_image = np.transpose(input_image, index_tup)
-        current_axes = 'bcyx'
-        resized_image = np.array([[cv2.resize(img_xy, (224, 224)) for img_xy in img_cxy] for img_cxy in input_image])
+        current_axes = output_format_sorted
+        resized_image = np.array([[cv2.resize(img_xy, output_dims_xy) for img_xy in img_cxy] for img_cxy in input_image])
         if resized_image.shape[0] > 1:  # flatten the 'b' dimension
-            resized_image = np.mean(resized_image, axis=0, keepdims=True)
-        num_channels = resized_image.shape[1]
-        if num_channels == 1:
-            # Grayscale to RGB: Repeat the single channel 3 times
-            resized_image = np.repeat(resized_image, 3, axis=1)
-        elif num_channels == 2:
-            # If there are 2 channels, add a third channel by repeating one of the existing ones
-            # Adding a third channel by repeating the second channel
-            resized_image = np.concatenate((resized_image, resized_image[:, 1:2, :, :]), axis=1)
-        elif num_channels > 3:
-            # If there are more than 3 channels, select the first 3 channels
-            resized_image = resized_image[:, :3, :, :]
+            resized_image = np.mean(resized_image, axis=0, keepdims='b' in output_format)
+        
+        final_img = paint_image(resized_image, input_format = output_format_sorted, output_channels = 3)
+        
+        # num_channels = resized_image.shape[1]
+        # if num_channels == 1:
+        #     # Grayscale to RGB: Repeat the single channel 3 times
+        #     resized_image = np.repeat(resized_image, 3, axis=1)
+        # elif num_channels == 2:
+        #     # If there are 2 channels, add a third channel by repeating one of the existing ones
+        #     # Adding a third channel by repeating the second channel
+        #     resized_image = np.concatenate((resized_image, resized_image[:, 1:2, :, :]), axis=1)
+        # elif num_channels > 3:
+        #     # If there are more than 3 channels, select the first 3 channels
+        #     resized_image = resized_image[:, :3, :, :]
+        
+        
         output_tup = tuple(current_axes.index(c) for c in output_format) # reshape to output format
         resized_image = np.transpose(resized_image, output_tup)
         return resized_image
@@ -421,7 +431,7 @@ class BioengineRunner():
 
 
 if __name__ == "__main__":
-    input_img = "./tmp/content.png"
+    input_img = "./tmp/user_input.jpeg"
     input_axes = "yxc"
     db_path = "./tmp/image_db"
     image_processor = ImageProcessor()
