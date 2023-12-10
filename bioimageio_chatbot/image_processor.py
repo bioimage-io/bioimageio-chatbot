ed972d9f810480df7922fdb8bf0d42941090dbba
gkreder <gkreder@gmail.com>
2023-12-10 22:04:08 +0100

    distance issues testing cellpose test set

--- bioimageio_chatbot/image_processor.py
+++ bioimageio_chatbot/image_processor.py
@@ -2,6 +2,9 @@ import os
 import typing as T
 import pickle as pkl
 import sys
+import asyncio
+import xml.etree.ElementTree as ET
+import randomname
 
 import scipy
 import matplotlib.pyplot as plt
@@ -15,6 +18,149 @@ from xarray import DataArray
 from tqdm.auto import tqdm
 from skimage import exposure
 import matplotlib as mpl
+from schema_agents.provider.openai_api import retry
+from pydantic import BaseModel, Field, validator
+from schema_agents.role import Role
+from schema_agents.schema import Message
+import shutil
+
+
+class AxisGuess(BaseModel):
+    """The best guess for what each axis in the image corresponds to. The largest dimensions will be 'x' and 'y'. The 'c' dimension will not be larger than 5. The numbers of dimensions in the shape tuple MUST match the number of axis labels"""
+    labels : list[str] = Field(description = f"The axis label for each dimension in the image's shape.")
+
+class UnlabeledImage(BaseModel):
+    """An input image"""
+    shape : list[int] = Field(description="The image's shape")
+
+class LabeledImage(UnlabeledImage):
+    """An image whose axes have been labeled according to its shape e.g. ['b','c','y','x']. The labels should intuitively make sense. The length of this list MUST exactly match the number of dimensions in the image's shape. You should avoid using the label 't' if possible. The 'c' dimension should not be larger than 5"""
+    axes : AxisGuess = Field(description = "A list representing the axes of the image (for example ['c','y','x'], ['b','x','y','c']). Each entry corresponds to the axis label of the image along the corresponding dimension. 't' is the least likely axis to appear in any image. The length of this string MUST match the number of dimensions in the image's `shape`. If in doubt between labeling a dimension as 'z' or 'c', 'c' should be assigned to the smaller dimension.")
+    @validator('axes')
+    def validate_axes_length(cls, v, values):
+        if 'shape' in values:
+            if len(v.labels) != len(values['shape']):
+                raise ValueError(f"The number of characters in the axes label string MUST exact match the number of dimensions in the image's shape. The number of dimensions in {values['shape']} is {len(values['shape'])} but the number of characters in the axes label string ({v}) is {len(v.labels)}")
+            if 'c' in v.labels and values['shape'][v.labels.index('c')] > 5:
+                raise ValueError(f"Error, the number of channels ('c' dimension) should not be greater than 5.")
+        for c in v.labels:
+            if c not in ['b', 'x', 'y', 'z', 'c', 't']:
+                raise ValueError("Please confine your axis labels to the characters 'b', 'x', 'y', 'z', 'c', 't'")
+        if 't' in v.labels and np.any([c not in v.labels for c in ['z', 'b', 'x', 'y']]):
+            raise ValueError("Please prioritize using 'z', 'b', 'x', or 'y' over 't' as an axis label. The label 't' should only be used as as last resort.")
+        if np.any([v.labels.count(c) > 1 for c in v.labels]):
+            raise ValueError("Every unique character can be used only once in the axes labels")
+        return v
+    
+class UnlabeledImages(BaseModel):
+    """A list of unlabeled images"""
+    unlabeled_images : list[UnlabeledImage] = Field(description="The unlabled images")
+
+class LabeledImages(BaseModel):
+    """A list of images whose axes have been labeled"""
+    labeled_images : list[LabeledImage] = Field(description="The labeled images")
+
+async def guess_image_axes(image : UnlabeledImage, role : Role = None) -> LabeledImage:
+    """Guesses the axis labels based on the image shape. The largest dimensions will be 'x' and 'y'. The 'c' dimension will not be larger than 5. The numbers of dimensions in the shape tuple must the number of axis labels"""
+    # response = await role.aask(image, LabeledImage)
+    response = role.aask(image.shape, AxisGuess)
+    labeled_image = LabeledImage(shape = image.shape, axes = response.axes)
+    return(labeled_image)
+
+async def retry_aask(role, ui, output_type):
+    @retry(5)
+    async def inner():
+        return await role.aask(ui, output_type)
+    return await inner()
+
+async def guess_all_axes(unlabeled_images : UnlabeledImages, role : Role = None) -> LabeledImages:
+    """Labels the axes in all images in the input list of unlabeled images"""
+    labeled_images = []
+    for unlabeled_image in unlabeled_images.unlabeled_images:
+        labeled_image = await role.aask(unlabeled_image, LabeledImage)
+        labeled_images.append(labeled_image)
+    
+    guessing_tasks = (retry_aask(role, ui, LabeledImage) for ui in unlabeled_images.unlabeled_images)
+    labeled_images = await asyncio.gather(*guessing_tasks)
+    labeled_images = LabeledImages(labeled_images=labeled_images)
+    return(labeled_images)
+
+def create_svg_table_unknown(input_images, shapes, guessed_axes):
+    ET.register_namespace("", "http://www.w3.org/2000/svg")
+    row_height = 20
+    column_widths = [200, 120, 120]  # Adjust as needed
+    header_height = 30
+    svg_height = header_height + row_height * len(input_images)
+    svg = ET.Element(ET.QName("http://www.w3.org/2000/svg", 'svg'), width='1000', height=str(svg_height))
+    ET.SubElement(svg, 'style').text = 'text { font-family: Arial; }'
+    def add_text(parent, x, y, text, font_size='12'):
+        element = ET.SubElement(parent, 'text', x=str(x), y=str(y), 
+                                style=f'font-size:{font_size}px;', 
+                                **{'text-anchor': 'start'})
+        element.text = text
+    # Add table headers
+    headers = ["Image", "Shape", "Guessed Axes"]
+    x_position = 0
+    for i, header in enumerate(headers):
+        add_text(svg, x_position + 5, header_height - 10, header, font_size='14')
+        x_position += column_widths[i]
+    # Add rows for each image
+    for i, (img, shape, guessed_ax) in enumerate(zip(input_images, shapes, guessed_axes)):
+        y_position = header_height + i * row_height
+        row_color = "#fffbfe"
+        ET.SubElement(svg, 'rect', x='0', y=str(y_position), 
+                      width='1000', height=str(row_height), 
+                      fill=row_color)
+        x_position = 0
+        for j, data in enumerate([img.split('/')[-1], str(shape), guessed_ax]):
+            add_text(svg, x_position + 5, y_position + 15, data)
+            x_position += column_widths[j]
+    return ET.tostring(svg, encoding='unicode')
+
+def create_svg_table_known(input_images, true_shapes, true_axes, guessed_axes):
+    ET.register_namespace("", "http://www.w3.org/2000/svg")
+    # Constants for table layout
+    row_height = 20
+    column_widths = [200, 120, 100, 120]  # Adjust as needed
+    header_height = 30
+    # Calculate SVG height based on the number of rows
+    svg_height = header_height + row_height * len(input_images)
+    # Create SVG element
+    svg = ET.Element(ET.QName("http://www.w3.org/2000/svg", 'svg'), width='1000', height=str(svg_height))
+    ET.SubElement(svg, 'style').text = 'text { font-family: Arial; }'
+    # Function to add a text element
+    def add_text(parent, x, y, text, font_size='12'):
+        element = ET.SubElement(parent, 'text', x=str(x), y=str(y), 
+                                style=f'font-size:{font_size}px;', 
+                                **{'text-anchor': 'start'})
+        element.text = text
+    # Add table headers
+    headers = ["Image", "True Shape", "True Axes", "Agent Guessed Axes"]
+    x_position = 0
+    for i, header in enumerate(headers):
+        add_text(svg, x_position + 5, header_height - 10, header, font_size='14')
+        x_position += column_widths[i]
+    # Add rows for each image
+    for i, (img, shape, true_ax, guessed_ax) in enumerate(zip(input_images, true_shapes, true_axes, guessed_axes)):
+        y_position = header_height + i * row_height
+        # row_color = '#90ee90' if true_ax == guessed_ax else '#ffcccb'  # Lighter shades of green and red
+        if true_ax == guessed_ax:
+            row_color = '#90ee90' # light green
+        elif true_ax.replace('x', '[').replace('y', 'x').replace('[', 'y') == guessed_ax: # (everything is same except for x and y)
+            row_color = "#ccccff" # light blue
+        else:
+            row_color = '#ffcccb' # light red
+        # Background color for row
+        ET.SubElement(svg, 'rect', x='0', y=str(y_position), 
+                      width='1000', height=str(row_height), 
+                      fill=row_color)
+        # Each column in a row
+        x_position = 0
+        for j, data in enumerate([img.split('/')[-1], str(shape), true_ax, guessed_ax]):
+            add_text(svg, x_position + 5, y_position + 15, data)
+            x_position += column_widths[j]
+    # Convert to string
+    return ET.tostring(svg, encoding='unicode')
 
 
 class Embedder():
@@ -267,6 +413,42 @@ def get_torch_db(db_path: str, processor: ImageProcessor, force_build: bool = Fa
         for img in model_torch_images:
             vec = processor.embed_image(img.numpy(), "bcyx")
             all_embeddings.append(vec)
+    # db_old = pkl.load(open(out_db_path, 'rb'))
+    # all_torch_images, all_embeddings, all_metadata = zip(*db_old)
+    # all_torch_images = list(all_torch_images)
+    # all_embeddings = list(all_embeddings)
+    # all_metadata = list(all_metadata)
+
+    cellpose_dir = os.path.join(db_path, 'cellpose_train-set_representatives')
+    cellpose_temp_dir = os.path.join(db_path, "cellpose_tmp")
+    if not os.path.exists(cellpose_dir):
+        os.makedirs(cellpose_temp_dir, exist_ok=True)
+        cellpose_drive_id = "1kEy9DHxkGeQHQDQco8CyaL1KEEPS6NYY"
+        cellpose_zip_file = os.path.join(db_path, "cellpose_train_reps.zip")
+        wget_cmd = f"wget --no-check-certificate 'https://docs.google.com/uc?export=download&id={cellpose_drive_id}' -O {cellpose_zip_file}"
+        os.system(wget_cmd)
+        unzip_cmd = f"unzip {cellpose_zip_file} -d {cellpose_temp_dir}"
+        os.system(unzip_cmd)
+        shutil.move(os.path.join(cellpose_temp_dir, "cellpose_train-set_representatives"), db_path)
+        shutil.rmtree(cellpose_temp_dir, ignore_errors=True)
+
+    cellpose_input_images = [os.path.join(cellpose_dir, x) for x in os.listdir(cellpose_dir) if x.endswith('.png')]
+    for input_image_path in cellpose_input_images:
+        rn = randomname.get_name(noun=("cats", "dogs", "birds", "fish"))
+        nickname = f"cellpose_{rn}"
+        rdf_dict = {"config" : {'bioimageio' : {'nickname' : nickname}},
+                    'test_inputs' : os.path.abspath(input_image_path),
+                    "inputs" : [{'axes' : 'yxc'}]}
+        torch_image = processor.get_torch_image
+        input_image = processor.read_image(input_image_path)
+        resized_image = processor.resize_image(input_image, 'yxc', grayscale=grayscale, output_format = "bcyx")
+        resized_image = resized_image.astype(np.float32)
+        torch_image = torch.from_numpy(resized_image).to(torch.float32)
+        all_torch_images.extend([torch_image])
+        all_metadata.extend([rdf_dict])
+        vec = processor.embed_image(torch_image.numpy(), "bcyx")
+        all_embeddings.append(vec)
+
     db = list(zip(all_torch_images, all_embeddings, all_metadata))
     with open(out_db_path, 'wb') as f:
         pkl.dump(db, f)
@@ -392,7 +574,7 @@ def transform_input(image: np.ndarray, image_axes: str, output_axes: str):
     """
     return map_axes(image, image_axes, output_axes)
 
-def guess_image_axes(shape : tuple[int]):
+def guess_image_axes_deterministic(shape : tuple[int]):
     axes = []
     common_channel_sizes = [1, 3, 4, 5]  # Common channel sizes are 1 (grayscale), 3 (RGB), and 4 (RGBA)
     shapes_left = sorted([[x, i_x] for i_x, x in enumerate(shape)], key = lambda tup : tup[0], reverse = True, )
@@ -529,69 +711,21 @@ def min_distance_to_representative(vector : list, representative_db : list):
         distance = scipy.spatial.distance.cosine(vector, rep_vector)
         if distance < min_distance:
             min_distance = distance
-    return min_distance
+    return min_distance    
 
-def draft_get_representative_images(input_files : list, verbose : bool = False,
+async def get_representative_images(input_files : list, verbose : bool = False,
                               grayscale : bool = True, random_seed : int = 2010,
-                              threshold_distance : float = 0.25, safety_iter : int = 100):
-    image_processor = ImageProcessor()
-    embedded_db = []
-    if verbose:
-        print(f"Embedding input images")
-        sys.stdout.flush()
-    input_files_interable = tqdm(input_files) if verbose else input_files
-    for input_image_path in input_files_interable:
-        fname = os.path.basename(input_image_path)
-        input_image = image_processor.read_image(input_image_path)
-        input_axes = guess_image_axes(input_image.shape)
-        embedded_image = image_processor.embed_image(input_image, input_axes, grayscale = grayscale)
-        embedded_db.append((fname, input_image_path, embedded_image))
-
-    n = len(embedded_db)
-    distances = np.zeros((n, n))
-    if verbose:
-        print('Calculating distance matrix')
-        sys.stdout.flush()
-    dist_iterable = tqdm(range(n)) if verbose else range(n)
-    for i in dist_iterable:
-        for j in range(n):
-            if i != j:
-                cos_dist = scipy.spatial.distance.cosine(embedded_db[i][2], embedded_db[j][2])
-                distances[i, j] = cos_dist
-                distances[j, i] = cos_dist
-            else:
-                distances[i, j] = 0.0
-    
-    np.random.seed(random_seed)
-    first_choice = np.random.choice(n, replace=False)
-    # representative_db = [embedded_db[first_choice]]
-    representative_indices = [first_choice]
-    current_dists = np.array([distances[x].max() for x in representative_indices])
-    current_idxs = np.array([distances[x].argmax() for x in representative_indices])
-
-    if verbose:
-        print('Assembling representative set')
-    rep_iterable = tqdm(range(n)) if verbose else range(n)
-    for i in rep_iterable:
-        i_max = current_dists.argmax()
-        max_dist = current_dists[i_max]
-        if max_dist < threshold_distance:
-            break
-
-        new_rep_idx = current_idxs[i_max]
-        representative_indices.append(new_rep_idx)
-        distances[:, new_rep_idx] = np.inf
-        distances[new_rep_idx, :] = np.inf
-        current_dists = np.array([distances[x].max() for x in representative_indices])
-        current_idxs = np.array([distances[x].argmax() for x in representative_indices])
-    
-    representative_db = [embedded_db[idx] for idx in representative_indices]
-    return([v[1] for v in representative_db])
-    
-
-def get_representative_images(input_files : list, verbose : bool = False,
-                              grayscale : bool = True, random_seed : int = 2010,
-                              threshold_distance : float = 0.25, safety_iter : int = 100):
+                              threshold_distance : float = 0.25, safety_iter : int = 100,
+                              llm_axis_guessing = True):
+    axis_guesser = Role(name = "AxisGuesser",
+                profile = "Axis Guesser",
+                goal="Your goal as AxisGuesser is read the shapes of input images and guess their axis labels using common sense.",
+            constraints=None,
+            actions=[guess_image_axes, guess_all_axes])
+    event_bus = axis_guesser.get_event_bus()
+    event_bus.register_default_events()
+    guessed_axes = []
+    shapes = []
     image_processor = ImageProcessor()
     embedded_db = []
     input_files_interable = tqdm(input_files) if verbose else input_files
@@ -600,7 +734,16 @@ def get_representative_images(input_files : list, verbose : bool = False,
     for input_image_path in input_files_interable:
         fname = os.path.basename(input_image_path)
         input_image = image_processor.read_image(input_image_path)
-        input_axes = guess_image_axes(input_image.shape)
+        shapes.append(input_image.shape)
+    if llm_axis_guessing:
+        message_input = UnlabeledImages(unlabeled_images = [UnlabeledImage(shape = s) for s in shapes])
+        m = Message(content = 'guess the image axes for each image in the list', data = message_input, role = 'User')
+        responses = await axis_guesser.handle(m)
+        guessed_axes = [''.join(x.axes.labels) for x in responses[0].data.labeled_images]
+    else:
+        guessed_axes = [guess_image_axes_deterministic(s) for s in shapes]
+    for i_img, input_image_path in enumerate(input_files_interable):
+        input_axes = guessed_axes[i_img]
         embedded_image = image_processor.embed_image(input_image, input_axes, grayscale = grayscale)
         embedded_db.append((fname, input_image_path, embedded_image))
     np.random.seed(random_seed)
