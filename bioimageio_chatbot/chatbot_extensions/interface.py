a9cb437cd888867e12536f7aec9737762192a808
Wei Ouyang <oeway007@gmail.com>
2023-12-17 00:24:54 +0800

    Clean up cellpose

--- bioimageio_chatbot/chatbot_extensions/interface.py
+++ bioimageio_chatbot/chatbot_extensions/interface.py
@@ -44,16 +44,3 @@ if __name__ == "__main__":
     print(json_file_path)
     extension_objects = load_extensions_from_json(json_file_path)
     print(extension_objects)
-
-
-
-# def get_modes():
-#     cellpose_description = "Runs Cellpose image segmentation (either cytoplasm or nuclei) on user images using pretrained models"
-#     mode_dict = {}
-#     for mode, channel_name, response_function, description in [[CellposeTask, 'Cellpose Image Analyzer', cellpose_get_response, cellpose_description]]:
-#         mode_dict[channel_name] = {
-#             'mode': mode,
-#             'response_function': response_function,
-#             'description': description
-#         }
-#     return mode_dict
