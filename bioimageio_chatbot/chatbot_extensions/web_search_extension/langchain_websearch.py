f1194d8a6e3187f531d3ba946248fdd6f80717e4
Wei Ouyang <oeway007@gmail.com>
2024-02-15 00:57:46 +0100

    Improve web search

--- bioimageio_chatbot/chatbot_extensions/web_search_extension/langchain_websearch.py
+++ bioimageio_chatbot/chatbot_extensions/web_search_extension/langchain_websearch.py
@@ -1,7 +1,8 @@
 import re
-import concurrent.futures
+import asyncio
+from typing import Union
 
-import requests
+import httpx
 from bs4 import BeautifulSoup
 from langchain_community.document_transformers import EmbeddingsRedundantFilter
 from langchain.retrievers.document_compressors import DocumentCompressorPipeline
@@ -30,23 +31,28 @@ class LangchainCompressor:
         text = text.strip()
         return text
 
-    def faiss_embedding_query_urls(self, query: str, url_list: list[str], num_results: int = 5,
+    async def faiss_embedding_query_urls(self, query: str, url_list: list[str], num_results: int = 5,
                                    similarity_threshold: float = 0.5, chunk_size: int = 500) -> list[Document]:
-        html_url_tupls = []
+        html_url_tuples = []
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            future_to_url = {executor.submit(download_html, url): url for url in url_list}
-            for future in concurrent.futures.as_completed(future_to_url, timeout=10):
-                url = future_to_url[future]
-                try:
-                    html_url_tupls.append((future.result(), url))
-                except Exception as exc:
-                    print('LLM_Web_search | %r generated an exception: %s' % (url, exc))
+        # Creating a list of tasks for each URL
+        tasks = [download_html(url) for url in url_list]
 
-        if not html_url_tupls:
+        # Using asyncio.gather to run all tasks concurrently
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+
+        # Processing results and exceptions
+        for result, url in zip(results, url_list):
+            if isinstance(result, Exception):
+                print(f'An exception occurred for {url}: {result}')
+            else:
+                html_url_tuples.append((result, url))
+
+        if not html_url_tuples:
             return []
 
-        documents = [html_to_plaintext_doc(html, url) for html, url in html_url_tupls]
+        documents = [html_to_plaintext_doc(html, url) for html, url in html_url_tuples]
+        
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=10,
                                                        separators=["\n\n", "\n", ".", ", ", " ", ""])
         split_docs = text_splitter.split_documents(documents)
@@ -77,7 +83,7 @@ class LangchainCompressor:
             retrievers=[bm25_retriever, compression_retriever], weights=[0.4, 0.5]
         )
 
-        compressed_docs = ensemble_retriever.get_relevant_documents(query)
+        compressed_docs = await ensemble_retriever.aget_relevant_documents(query)
 
         # Ensemble may return more than "num_results" results, so cut off excess ones
         return compressed_docs[:num_results]
@@ -91,22 +97,23 @@ def docs_to_pretty_str(docs) -> str:
         ret_str += f"Source URL: {doc.metadata['source']}\n\n"
     return ret_str
 
+async def download_html(url: str) -> bytes:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5"
+    }
 
-def download_html(url: str) -> bytes:
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
-               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-               "Accept-Language": "en-US,en;q=0.5"}
-
-    response = requests.get(url, headers=headers, verify=True, timeout=8)
-    response.raise_for_status()
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers, timeout=8)
+        response.raise_for_status()
 
     content_type = response.headers.get("Content-Type", "")
     if not content_type.startswith("text/html"):
         raise ValueError(f"Expected content type text/html. Got {content_type}.")
     return response.content
 
-
-def html_to_plaintext_doc(html_text: str or bytes, url: str) -> Document:
+def html_to_plaintext_doc(html_text: Union[str, bytes], url: str) -> Document:
     soup = BeautifulSoup(html_text, features="lxml")
     for script in soup(["script", "style"]):
         script.extract()
